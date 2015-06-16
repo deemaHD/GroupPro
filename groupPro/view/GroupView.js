d3fad96306b0849105067a6f135220a71f86ed44
deemaHD <dima.selezen@rambler.ru>
2015-06-16 21:57:20 +0300

    third commit

--- groupPro/view/GroupView.js
+++ groupPro/view/GroupView.js
@@ -25,19 +25,19 @@ function GroupView () {
     function addEvents (event) {
         if (event.target.innerHTML === students[0].getAttribute('name')) {
             argument = students[0].toJSON();
-        } else if (event.target.innerHTML == students[1].getAttribute('name')) {
+        } else if (event.target.innerHTML === students[1].getAttribute('name')) {
             argument = students[1].toJSON();
-        } else if (event.target.innerHTML == students[2].getAttribute('name')) {
+        } else if (event.target.innerHTML === students[2].getAttribute('name')) {
             argument = students[2].toJSON();
-        } else if (event.target.innerHTML == students[3].getAttribute('name')) {
+        } else if (event.target.innerHTML === students[3].getAttribute('name')) {
             argument = students[3].toJSON();
-        } else if (event.target.innerHTML == students[4].getAttribute('name')) {
+        } else if (event.target.innerHTML === students[4].getAttribute('name')) {
             argument = students[4].toJSON();
-        } else if (event.target.innerHTML == students[5].getAttribute('name')) {
+        } else if (event.target.innerHTML === students[5].getAttribute('name')) {
             argument = students[5].toJSON();
-        } else if (event.target.innerHTML == students[6].getAttribute('name')) {
+        } else if (event.target.innerHTML === students[6].getAttribute('name')) {
             argument = students[6].toJSON();
-        } else if (event.target.innerHTML == students[7].getAttribute('name')) {
+        } else if (event.target.innerHTML === students[7].getAttribute('name')) {
             argument = students[7].toJSON();
         }
         mediator.publish('studentSelect', argument);
